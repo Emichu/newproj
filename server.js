f1d53754f29df42a207d7a4feae010d7a2bf5c8c
Emichu <k.kreplewicz@gmail.com>
2019-10-12 12:40:47 +0100

    another fix for heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
+const host = '0.0.0.0';
 
 app.get('/', (req, res) => res.send('Hello World'));
 
-app.set('port', port);
-app.listen(() => console.log(`listening on port ${port}`));
+app.listen(port, host, () => console.log(`listening on port ${port}`));
