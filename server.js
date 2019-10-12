d4c19523796aeae0c7b6367e6ba8c8625b2079ca
Emichu <k.kreplewicz@gmail.com>
2019-10-12 16:13:28 +0100

    added pug views

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+require 
 const app = express();
 const port = process.env.PORT || 80;
 const host = '0.0.0.0';
 
-app.get('/', (req, res) => res.send('Hello World'));
-
 app.listen(port, host, () => console.log(`listening on port ${port}`));
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
