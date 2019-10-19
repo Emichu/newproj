1b27eb327b3317ef7343d38928196bc9134624c7
Emichu <k.kreplewicz@gmail.com>
2019-10-19 14:45:33 +0100

    live list of repos from my profile

--- server.js
+++ server.js
@@ -1,43 +1,14 @@
 const express = require('express');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const app = express();
+const axios = require("axios")
 const port = process.env.PORT || 80;
 const host = '0.0.0.0';
-const request = new XMLHttpRequest();
 
 app.listen(port, host, () => console.log(`listening on port ${port}`));
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-
-var repos = [
-    {
-        name: "API-list",
-        link: "http://www.google.com"
-    },
-    {
-        name: "newproj",
-        link: "http://www.google.com"
-    }
-];
-
-app.get('/', function (req, res) {
-    res.render('index', {collection: repos})
-  })
-
-// request.open('GET', 'https://api.github.com/users/Emichu/repos', true);
-
-// request.onload = function() {
-//     if(request.status !== 200){
-//         console.log('error');
-//         return;
-//     }
-
-//     var data = JSON.parse(this.response);
-
-//     data.forEach(function(repo){
-//         var content = res.reder('index', {title: 'GitHub Repos', message: 'GitHub Repos', list: repo.name})
-//     });
-// }
-
-// request.send()
+app.get("/", async function(req, res){
+    const resObj = await axios.get("https://api.github.com/users/Emichu/repos");
+    res.render('index', { collection: resObj.data })
+});
