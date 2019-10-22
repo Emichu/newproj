b817a6bfc2299b70d962fa3b33669488c966eb5e
Emichu <k.kreplewicz@gmail.com>
2019-10-22 20:43:53 +0100

    live repo list with fancier links

--- server.js
+++ server.js
@@ -13,3 +13,23 @@ app.get("/", async function(req, res){
     const resObj = await axios.get("https://api.github.com/users/Emichu/repos");
     res.render('index', { collection: resObj.data })
 });
+
+app.get("/repos/:id", async function(req, res) {
+    /*
+        get all the repos
+        filter array to find repo with id that matches the param
+        pass repo obj into view
+    */
+    const resObj = await axios.get("https://api.github.com/users/Emichu/repos")
+    const repos = resObj.data
+
+    const repo = repos.filter(function(repo){
+        return repo.id === Number(req.params.id)
+    });
+
+    if(repo.length === 0) {
+        res.render('errorpg')}
+    else {
+        res.render('dummypage', {repo: repo[0]})
+    }
+});
